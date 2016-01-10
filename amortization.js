f0b3f8962c5d7b784f442b10b820a897896c000b
Andrés Estévez <aecostas@gmail.com>
2016-01-10 13:50:01 +0100

    Added support to partial amortizations via REST

--- amortization.js
+++ amortization.js
@@ -38,8 +38,9 @@ function amortized(payment, interest, paidmonths, numberofpayments) {
  * @param {object} partial_amortization - 
  */
 function performPartialAmortization(partial_amortization, currentMonth) {
-    if (currentMonth == 0) return false
-
+    if (currentMonth == 0) {
+	return false
+    } 
     return ((partial_amortization.type == "periodic") &&
 	    ((currentMonth % partial_amortization.period) == 0)) ||
 	((partial_amortization.type == "extra") &&
@@ -54,7 +55,7 @@ function performPartialAmortization(partial_amortization, currentMonth) {
  * @param {int} term - Mortgage term (i.e.: number of total months)
  * @param {object} partial_amortization - 
  */
-function calculate(mortgage, interest, term, partial_amortization) {
+function calculate(mortgage, interest, term, partial_amortizations) {
     let N = term
     let currentMonth=1
     let capital = mortgage
@@ -65,12 +66,16 @@ function calculate(mortgage, interest, term, partial_amortization) {
     while (N>0) {
 	let extra=0
 	let a_n_new = amortized(payment, interest, currentMonth-1, currentMonth-1+N)
+	// TODO: should a_n be calculated before performing amortizations
 	let a_n = capital - a_n_new
 	capital = a_n_new
-	if (performPartialAmortization(partial_amortization, currentMonth)) {
-	    capital -= partial_amortization.amount
-	    N = updateNumberOfPayments(capital, payment, interest)
-	    extra=partial_amortization.amount
+
+	for (var amort in partial_amortizations) {
+	    if (performPartialAmortization(partial_amortizations[amort], currentMonth)) {
+		capital -= partial_amortizations[amort].amount
+		N = updateNumberOfPayments(capital, payment, interest)
+		extra += partial_amortizations[amort].amount
+	    }
 	}
 
 	sumInterest+=payment - a_n;
@@ -93,20 +98,28 @@ function calculate(mortgage, interest, term, partial_amortization) {
 
 }// calculate
 
-
-
 app.get('/', function (req, res) {
     let _mortgage
     let _interest
     let _monthlyInterest
     let _term
     let _amortization
+    let _partial
     let _sumacuotas = 0
 
     _mortgage = req.query['mortgage']
     _interest = req.query['interest']
     _term = req.query['term']
-//    _amortization = JSON.parse(req.query['amortization'])
+
+    if (typeof(req.query['partial']) == 'undefined') {
+	console.warn('No amortization data')
+    } else if (typeof(req.query['partial']) == 'object' ) {
+	_partial = req.query['partial'].map(function(i){return JSON.parse(i)})	
+    } else {
+	// converting to array to access it in the same
+	// way regardless the number of elements
+	_partial = [JSON.parse(req.query['partial'])]
+    }
 
     if ((_mortgage === undefined) || 
 	(_term === undefined) ||
@@ -114,21 +127,25 @@ app.get('/', function (req, res) {
 	res.status(400)
 	res.send("Bad request - term, mortgage or interest undefined")
 	return
-    }    
+    }
 
-    let partial = {type: "periodic", period: 3, amount: 1000}
-    let partial2 = {type: "extra", month: 10, amount: 1000}
-    let partial3 = {}
+    if (_partial !== undefined) {
+	// TODO: check partial amortizations format
+	console.warn(_partial)
+    }
+
+    // let partial = {type: "periodic", period: 3, amount: 1000}
+    // let partial2 = {type: "extra", month: 10, amount: 1000}
+    // let partial3 = {}
 
     _monthlyInterest = _interest/12
 
-    let _payments = calculate(_mortgage, _monthlyInterest, _term, partial)
+    let _payments = calculate(_mortgage, _monthlyInterest, _term, _partial)
 
     let total = _payments.months.reduce(function(prev, current, index, vector) {
 	return {payment: prev.payment + current.payment + current.extra}
     });
 
-//    console.log(_payments)
     console.log ("Cuota: " + _payments.payment)
     console.log ("Hipoteca: " + _mortgage)
     console.log ("Pagado al banco: " + total.payment)
